24be26c39b77e2a3e4d86814c4e1c1067ce9bf9e
Khelil Sator <khelil.sator.git@gmail.com>
2018-03-12 12:16:21 +0100

    Update northstar_get_all_lsp.py

--- northstar_get_all_lsp.py
+++ northstar_get_all_lsp.py
@@ -4,12 +4,24 @@
 import requests
 from requests.auth import HTTPBasicAuth
 from pprint import pprint
+import yaml
 
+def import_variables_from_file():
+ my_variables_file=open('variables.yml', 'r')
+ my_variables_in_string=my_variables_file.read()
+ my_variables_in_yaml=yaml.load(my_variables_in_string)
+ my_variables_file.close()
+ return my_variables_in_yaml
+
+my_variables_in_yaml=import_variables_from_file()
+
+authuser = my_variables_in_yaml['northstar']['username']
+authpwd = my_variables_in_yaml['northstar']['password']
+url_base = 'http://' + my_variables_in_yaml['northstar']['ip'] + ':8091/NorthStar/API/v2/tenant/'
+
+url = url_base + '1/topology/1/te-lsps'
 headers = { 'Accept': 'application/json' }
 headers = { 'Content-type': 'application/json' }
-url='http://a.b.c.d:8091/NorthStar/API/v2/tenant/1/topology/1/te-lsps'
-authuser='xxxx'
-authpwd='yyyyy'
 
 r = requests.get(url, headers=headers, auth=(authuser, authpwd))
 
